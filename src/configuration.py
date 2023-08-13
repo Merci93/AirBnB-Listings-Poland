9f1a13595a8909088176aff7a34ad39f325de176
David Ugochukwu Asogwa <asogwadavidu@gmail.com>
2023-08-13 23:35:39 +0200

    configuration file

--- src/configuration.py
+++ src/configuration.py
@@ -1,3 +1,7 @@
 """Contains the directory/folder where the extracted data will be saved"""
 
-SAVE_DATA = r"C:\Users\David Ugochi Asogwa\Documents\Folders\GitHub\AirBnB_Listings_in_Poland"
+SAVE_EXTRACTED_DATA = r"C:\Users\David Ugochi Asogwa\Documents\Folders\GitHub\AirBnB_Listings_in_Poland\data"
+DATA_SOURCE = r"C:\Users\David Ugochi Asogwa\Documents\Folders\GitHub\AirBnB_Listings_in_Poland"
+LOG_DIRECTORY = r"C:\Users\David Ugochi Asogwa\Documents\Folders\GitHub\AirBnB_Listings_in_Poland\logs"
+
+URL = "https://www.airbnb.com/"
