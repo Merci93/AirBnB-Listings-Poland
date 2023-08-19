d2b6122c9c6c394fa22c0a0024b772085fdd6033
David Ugochukwu Asogwa <asogwadavidu@gmail.com>
2023-08-19 21:08:41 +0200

    configuration file

---
+++ src/configuration.py
@@ -0,0 +1,17 @@
+"""
+Contains the directory/folder where the extracted data will be saved.
+Modify as appropriate
+"""
+
+SAVE_EXTRACTED_DATA = path_to_save_extracted_data
+DATA_SOURCE = path_to_locate_csv_file_with_city_names
+LOG_DIRECTORY = path_to_log_directory
+
+URL = "https://www.airbnb.com"
+
+MONTH = "August"
+# MONTH = "September"
+# MONTH = "October"
+# MONTH = "November"
+# MONTH = "December"
+# MONTH = "January"
