b7754545821607b266ed6f33190947ea40f3bab1
David Ugochukwu Asogwa <asogwadavidu@gmail.com>
2023-08-20 08:58:39 +0200

    added path to save data info

--- src/configuration.py
+++ src/configuration.py
@@ -4,6 +4,7 @@ Modify as appropriate
 """
 
 SAVE_EXTRACTED_DATA = path_to_save_extracted_data
+SAVE_EXTRACTED_DATA_INFO = path_to_save_information_about_extracted_data
 DATA_SOURCE = path_to_locate_csv_file_with_city_names
 LOG_DIRECTORY = path_to_log_directory
 
