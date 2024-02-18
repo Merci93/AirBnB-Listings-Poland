844f77d11312933145847a6645e0556a9a14bbcc
David Ugochukwu Asogwa <asogwadavidu@gmail.com>
2024-02-18 18:56:23 +0100

    airflow dags directory

---
+++ dags/configuration.py
@@ -0,0 +1,9 @@
+"""Contains the directory/folder where the extracted data will be saved."""
+
+SAVE_EXTRACTED_DATA = r"C:\Users\David Ugochi Asogwa\Documents\Folders\GitHub\AirBnB_Listings_in_Poland\data"
+SAVE_EXTRACTED_DATA_INFO = r"C:\Users\David Ugochi Asogwa\Documents\Folders\GitHub\AirBnB_Listings_in_Poland\data\data_info"
+HTML_DATA = r"C:\Users\David Ugochi Asogwa\Documents\Folders\GitHub\AirBnB_Listings_in_Poland\data\html_data"
+DATA_SOURCE = r"C:\Users\David Ugochi Asogwa\Documents\Folders\GitHub\AirBnB_Listings_in_Poland"
+LOG_DIRECTORY = r"C:\Users\David Ugochi Asogwa\Documents\Folders\GitHub\AirBnB_Listings_in_Poland\logs"
+
+URL = "https://www.airbnb.com"
