b22c74adb9da08243d5e20554c4f494e1a46c7bd
David Ugochukwu Asogwa <asogwadavidu@gmail.com>
2024-02-25 20:35:03 +0100

    added data staging method

--- dags/load_data.py
+++ dags/load_data.py
@@ -1,6 +1,5 @@
 """A module to load the extracted data into PostgresSQL database."""
 
-import pandas as pd
 import psycopg2
 import yaml
 from pyspark.sql import DataFrame, SparkSession
@@ -15,15 +14,12 @@ class Connection:
 
         with open(connection_file, "r") as f:
             data = yaml.safe_load(f)
-        try:
-            self.connection = psycopg2.connect(dbname=data["db_name"],
-                                               user=data["user"],
-                                               password=data["password"],
-                                               host=data["host"],
-                                               port=data["port"],
-                                               )
-        except psycopg2.OperationalError:
-            raise "Unable to connect. Please check parameters"
+        self.staging_db = data["staging_db"]
+        self.main_db = data["main_db"]
+        self.user = data["user"]
+        self.password = data["password"]
+        self.host = data["host"]
+        self.port = data["port"]
     
     def close_connection(self) -> psycopg2:
         """Close active connections to the database."""
@@ -36,7 +32,49 @@ class Connection:
     def stage_data(self, data: list[dict[str]]) -> None:
         """Load data into the database."""
         df = self.transform_to_df(data)
-        pass
-    
-        
+
+        try:
+            connection = psycopg2.connect(dbname=self.staging_db,
+                                          user=self.user,
+                                          password=self.password,
+                                          host=self.host,
+                                          port=self.port,
+                                          )
+        except psycopg2.OperationalError:
+            raise "Unable to connect. Please check parameters"
+
+        cursor = connection.cursor()
+        cursor.execute("""
+                       CREATE TABLE IF NOT EXISTS data_staging (
+                           City VARCHAR(50),
+                           Date DATE,
+                           Title TEXT,
+                           Subtitle TEXT,
+                           Bed Type VARCHAR(20),
+                           Price per Night INT,
+                           Original Price: INT,
+                           Availability VARCHAR(20),
+                           Total Price INT,
+                           Stars INT,
+                           Ratings INT,
+                       );
+                       """)
+
+        insert_data = """INSERT INTO data_staging (
+            City, Date, Title, Subtitle, Bed Type, Price per Night, Original Price, Availability, Total Price, Stars, Ratings)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
+
+        # TODO Update this section for spark usage.
+        for _, row in df.iterrows():
+            cursor.execute(insert_data, row["city"], row["date"], row["title"], row["subtitle"], row["bed_type"],
+                           row["price_per_night (zl)"], row["original_price (zl)"], row["availability"],
+                           row["total_price (zl)"], row["stars"], row["number_of_ratings"])
+
+
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+        return True
     
