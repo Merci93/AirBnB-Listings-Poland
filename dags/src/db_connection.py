ab1ea7fbfe66cb668d59b480d120ca163e1e1e94
David Ugochukwu Asogwa <asogwadavidu@gmail.com>
2024-07-06 20:13:20 +0200

    small fix

--- dags/src/db_connection.py
+++ dags/src/db_connection.py
@@ -7,23 +7,20 @@ config.init_settings()
 
 
 class Connection:
-    def staging_db_connect(self) -> psycopg2.connect:
-        """Create connection to PostgreSQL staging database."""
-        try:
-            connection = psycopg2.connect(dbname=config.settings.staging_db,
-                                          user=config.settings.user,
-                                          password=config.settings.password,
-                                          host=config.settings.host,
-                                          port=config.settings.port,
-                                          )
-            return connection
-        except psycopg2.OperationalError:
-            raise psycopg2.OperationalError("Unable to connect. Please check parameters")
+    def database_connect(self, db_name: str) -> psycopg2.connect:
+        """
+        Create connection to PostgreSQL staging database.
 
-    def main_db_connect(self) -> psycopg2.connect:
-        """Create connection to PostgreSQL main database."""
+        :param: db_name: An indicator of which database to be connected to - staging or main
+        """
+        database_name = None
+        if db_name == "staging":
+            database_name = config.settings.staging_db
+        elif db_name == "main":
+            database_name = config.settings.main_db
+
         try:
-            connection = psycopg2.connect(dbname=config.settings.main_db,
+            connection = psycopg2.connect(dbname=database_name,
                                           user=config.settings.user,
                                           password=config.settings.password,
                                           host=config.settings.host,
@@ -35,8 +32,4 @@ class Connection:
 
     def close_staging_db_connection(self) -> psycopg2:
         """Close active connections to the staging database."""
-        self.staging_db_connect.close()
-
-    def close_main_db_connection(self) -> psycopg2:
-        """Close active connections to the main database."""
-        self.main_db_connect.close()
+        self.database_connect.close()
