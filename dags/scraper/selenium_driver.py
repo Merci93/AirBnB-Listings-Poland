c8de59d9ff3fdad03d6ae76fc07ba5c012550b34
David Ugochukwu Asogwa <asogwadavidu@gmail.com>
2024-08-12 01:07:10 +0200

    removed service install

--- dags/scraper/selenium_driver.py
+++ dags/scraper/selenium_driver.py
@@ -1,10 +1,6 @@
 """Selenium web driver module"""
-import os
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 def init_driver(run_headless: bool = True) -> webdriver.Chrome:
@@ -24,8 +20,5 @@ def init_driver(run_headless: bool = True) -> webdriver.Chrome:
                                         "profile.default_content_setting_values.notifications": 2,
                                     }
                                     )
-    chrome_install = ChromeDriverManager().install()
-    install_folder = os.path.dirname(chrome_install)
-    service = Service(os.path.join(install_folder, "chromedriver.exe"))
-    driver = webdriver.Chrome(service=service, options=options)
+    driver = webdriver.Chrome(options=options)
     return driver
